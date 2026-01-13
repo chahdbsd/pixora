835266c31209ae0908b887846073106fa11da6b0
BOUASSIDA Chahd <chahd.bouassida@et.esiea.fr>
2026-01-13 01:03:21 +0100

    Disable Kafka consumer on Render

--- src/main/java/com/pixora/frameworks/messaging/PhotoUploadedConsumer.java
+++ src/main/java/com/pixora/frameworks/messaging/PhotoUploadedConsumer.java
@@ -2,9 +2,15 @@ package com.pixora.frameworks.messaging;
 
 import com.pixora.usecase.event.PhotoUploadedEvent;
 import com.pixora.usecase.service.HandlePhotoUploadedEventUseCase;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@ConditionalOnProperty(
+        name = "KAFKA_ENABLED",
+        havingValue = "true",
+        matchIfMissing = true
+)
 @Component
 public class PhotoUploadedConsumer {
 
